4488bf4a79af52eec6e30e95abad30e3bd73e4c8
DurgaCSE11 <durgaprasadmahapatra21@gmail.com>
2026-06-11 19:24:17 +0530

    Add Skills and Resume pages, and adjust sidebar role formatting

--- src/components/nav/partials/NavProfileCard.jsx
+++ src/components/nav/partials/NavProfileCard.jsx
@@ -80,7 +80,7 @@ function NavProfileCard({ profile, expanded }) {
 
                 {roles?.length > 0 && (
                     <div className={`nav-profile-card-role`}
-                         dangerouslySetInnerHTML={{__html: roles.join(' | ')}}/>
+                         dangerouslySetInnerHTML={{__html: roles.join('<br/>')}}/>
                 )}
             </div>
         </Card>
