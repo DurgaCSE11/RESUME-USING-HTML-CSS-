8503a6ae1c536e2b9e8636888b67b6cae46ecf50
DurgaCSE11 <durgaprasadmahapatra21@gmail.com>
2026-06-12 14:04:59 +0530

    Remove translation capability and Google Translate script

--- src/components/nav/partials/NavToolList.jsx
+++ src/components/nav/partials/NavToolList.jsx
@@ -17,20 +17,7 @@ function NavToolList({ expanded }) {
     const data = useData()
 
     useEffect(() => {
-        const scriptId = 'google-translate-script';
-        if (!document.getElementById(scriptId)) {
-            window.googleTranslateElementInit = () => {
-                new window.google.translate.TranslateElement({
-                    pageLanguage: 'en',
-                    layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE
-                }, 'google_translate_element');
-            };
-            const script = document.createElement('script');
-            script.id = scriptId;
-            script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-            script.async = true;
-            document.body.appendChild(script);
-        }
+        // Translations disabled
     }, []);
 
     const profile = data.getProfile()
